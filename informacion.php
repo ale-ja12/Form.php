07054450ac82de5b095c06f49bdc6460e35a755a
ale-ja12 <alejandram.120609@gmail.com>
2025-09-13 08:51:20 -0500

    informacion.php

--- informacion.php
+++ informacion.php
@@ -3,8 +3,11 @@ $nombre = $_POST ['nombre'];
 $edad = $_POST ['edad'];
 $dirección = $_POST ['dirección'];
 $salario = $_POST ['salario'];
+$correo = $_POST ['correo'];
+$cargo = $_POST ['cargo'];
 
 echo "<h2>Información recibida</h2>";
 echo "El empleado $nombre tiene $edad años.<br>";
 echo "Vive en $dirección y su salario es de $salario.";
+echo "Su  correo es $correo y su cargo es $cargo.;
 ?>
